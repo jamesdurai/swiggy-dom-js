4c1fbd47de6993dca335bbc2195a46c18dc96dd5
jamesdurai <jamesdurai@outlook.com>
2023-06-27 10:29:24 +0530

    Update Swiggy Framework

---
+++ main.js
@@ -0,0 +1,348 @@
+let food = [
+  {
+    id: 1,
+    name: "Udupi Gardenia",
+    cuisine: "South Indian, North Indian",
+    imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/e5k8viofbtrxxo75lxdy",
+    ratings: "4.4",
+    preparation: 45,
+    amount: 350 ,
+    units:"for two",
+  },
+  {
+    id: 2,
+    name: "Srinidhi Veg Food Court",
+    cuisine: "South Indian",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/aqsnrylokzpn45qhg1pb",
+    ratings: "3.4",
+    preparation: 37,
+    amount: 200,
+    units:"for two",
+  },
+  {
+    id: 3,
+    name: "Third Wave Coffee",
+    cuisine: " Beverages, Bakery",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hpncnz3sfv3gigsukkts",
+    ratings: "2.2",
+    preparation: 25,
+    amount: 350 ,
+    units:"for two",
+  },
+  {
+    id: 4,
+    name: "Madurai Idly Shop",
+    cuisine: "South Indian",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wplrmo25ipisxvrta6hg",
+    ratings: "4.4",
+    preparation: 40,
+    eachone: "Rs.200 for two",
+  },
+  {
+    id: 5,
+    name: "Royal Donne Biryani",
+    cuisine: "Biryani, Chinese",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/4df3497f1460dfd1ecd8125222f6d362",
+    ratings: "3.9",
+    preparation: 45,
+    amount: 400 ,
+    units:"for two",
+  },
+  {
+    id: 6,
+    name: "McDonald's",
+    cuisine: "Burgers, Beverages",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ew1qjainpgvq72n0692h",
+    ratings: "4.4",
+    preparation: 35,
+    amount: 250 ,
+    units:"for two",
+  },
+  {
+    id: 7,
+    name: "Bakingo",
+    cuisine: " Beverages, Desserts",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/psoxrwvub31l7mrvmgfz",
+    ratings: "3.3",
+    preparation: 25,
+    amount: 400 ,
+    units:"for two",
+  },
+  {
+    id: 8,
+    name: "Mrs Iyengar's Kitchen",
+    cuisine: "South Indian, Jain",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/lkmvezahlf694qw0y8dr",
+    ratings: "4.0",
+    preparation: 40,
+    amount: 300 ,
+    units:"for two",
+  },
+  {
+    id: 9,
+    name: "Mani's Dum Biryani",
+    cuisine: "North Indian, Hyderabadi",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/b6c0620fe9f64ed2116cfd06dd19a354",
+    ratings: "3.9",
+    preparation: 45,
+    amount: 250 ,
+    units:"for two",
+  },
+  {
+    id: 10,
+    name: "Anu Momos Chinese Kitchen",
+    cuisine: "Chinese",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bgps9bd890sffcg3rjh1",
+    ratings: "2.4",
+    preparation: 30,
+    amount: 300 ,
+    units:"for two",
+  },
+  {
+    id: 11,
+    name: "Kolkata Famous Katti Rolls",
+    cuisine: "Punjabi",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xvbzqxwzmmc3htdhofwc",
+    ratings: "3.6",
+    preparation: 25,
+    amount: 350 ,
+    units:"for two",
+  },
+  {
+    id: 12,
+    name: "Beijing Bites",
+    cuisine: "Chinese, Thai",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/s8zp0a35gd7mibgeqdw4",
+    ratings: "4.2",
+    preparation: 40,
+    amount: 250 ,
+    units:"for two",
+  },
+
+  {
+    id: 13,
+    name: "Imperio Restaurant",
+    cuisine: "Chinese",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/a1629b9c7f59577a55411e94425c6d99",
+    ratings: "2.3",
+    preparation: 37,
+    amount: 350 ,
+    units:"for two",
+  },
+  {
+    id: 14,
+    name: "Temperature",
+    cuisine: "Beverages, American",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/yz7ffjxfwmgwgukp4w9w",
+    ratings: "4.0",
+    preparation: 55,
+    amount: 400 ,
+    units:"for two",
+  },
+  {
+    id: 15,
+    name: "Angel Mess",
+    cuisine: "Kerala, South Indian",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xk45atw1txieojraxowr",
+    ratings: "4.6",
+    preparation: 41,
+    amount: 250 ,
+    units:"for two",
+  },
+  {
+    id: 16,
+    name: "Cafe Graze-Plant Based Cafe",
+    cuisine: "Beverages, Juices",
+    imgUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/gta92coa4477as9eu2ms",
+    ratings: "3.2",
+    preparation: 53,
+    amount: 600 ,
+    units:"for two",
+  },
+];
+
+let cont = document.querySelector(".container");
+
+let cards = document.createElement("div");
+cards.classList.add("row");
+cont.appendChild(cards);
+itemRenderer(food); 
+
+function itemRenderer(inpObj) {
+  inpObj.forEach(function (e) {
+    let div = document.createElement("div");
+    div.classList.add("col");
+    cards.appendChild(div);
+
+    let div2 = document.createElement("div");
+    div2.classList.add("col-card");
+    div.appendChild(div2);
+
+    let img = document.createElement("img");
+    img.src = e.imgUrl;
+    div2.appendChild(img);
+
+    let head = document.createElement("h2");
+    head.innerText = e.name;
+    div2.appendChild(head);
+
+    let para = document.createElement("p");
+    para.innerText = e.cuisine;
+    para.classList.add("cusine-para");
+    div2.appendChild(para);
+
+    // =============INNER DIV==============
+    let innerdiv = document.createElement("div");
+    innerdiv.classList.add("inner-row");
+    div2.appendChild(innerdiv);
+
+    let icondiv = document.createElement("div");
+    icondiv.classList.add("icon-div");
+    innerdiv.appendChild(icondiv);
+
+    let icon = document.createElement("i");
+    icon.classList.add("bi");
+    icon.classList.add("bi-star-fill");
+    icondiv.appendChild(icon);
+
+    let atag = document.createElement("p");
+    atag.innerText = e.ratings;
+    icondiv.appendChild(atag);
+
+    ratings();
+    function ratings() {
+      if (e.ratings >= 4.0) {
+        icondiv.classList.add("icon-div-green");
+        
+      } else if (e.ratings >= 2.5 && e.ratings <= 3.9) {
+        icondiv.classList.add("icon-div-orange");
+       
+      } else if (e.ratings <= 2.4) {
+        icondiv.classList.add("icon-div-red");
+        
+      }
+    }
+
+    let innerhead = document.createElement("h4");
+    innerhead.innerText = e.preparation + " mins";
+    innerdiv.appendChild(innerhead);
+
+    let innerhead2 = document.createElement("h4");
+    innerhead2.innerText ="Rs."+ e.amount+" "+e.units;
+    innerdiv.appendChild(innerhead2);
+
+
+
+    let incDiv = document.createElement("p");
+    incDiv.classList.add("qnty-sec")
+    div2.appendChild(incDiv);
+
+    let decreDiv = document.createElement("span");
+    decreDiv.innerHTML = "-";
+    decreDiv.setAttribute("onclick", "sub(this)");
+    incDiv.appendChild(decreDiv);
+
+    let zeroDiv = document.createElement("span");
+    zeroDiv.classList.add("leastVal");
+    zeroDiv.innerHTML = "0";
+    incDiv.appendChild(zeroDiv);
+
+    let increDiv = document.createElement("span");
+    increDiv.innerHTML = "+";
+    increDiv.setAttribute("onclick", "add(this)");
+    incDiv.appendChild(increDiv);
+  });
+}
+
+
+function sortTime() {
+  let sortTimeObj = food.slice().sort(function (a, b) {
+    return a.preparation - b.preparation;
+  });
+  let colsRemove = document.querySelectorAll(".col");
+  colsRemove.forEach((e) => {
+    e.remove();
+  });
+  itemRenderer(sortTimeObj);
+}
+
+function defaultSorter() {
+  let colsRemove = document.querySelectorAll(".col");
+  colsRemove.forEach((e) => {
+    e.remove();
+  });
+  itemRenderer(food);
+}
+
+
+function sortRatings() {
+  let sortRatingObj = food.slice().sort(function (a, b) {
+    return b.ratings - a.ratings;
+  });
+  let colsRemove = document.querySelectorAll(".col");
+  colsRemove.forEach((e) => {
+    e.remove();
+  });
+  itemRenderer(sortRatingObj);
+}
+
+function filterCusine() {
+  let colsRemove = document.querySelectorAll(".col");
+  colsRemove.forEach((e) => {
+    e.remove();
+  });
+  let filterCusineObj = food.filter(function (e) {
+    console.log(e.cuisine);
+    return e.cuisine == "South Indian";
+  });
+  itemRenderer(filterCusineObj);
+  console.log(filterCusineObj);
+}
+
+function add(e) {
+  e.parentElement.childNodes.forEach(function (cv) {
+  if(cv.classList.contains('leastVal')){
+    var a = parseInt(cv.innerHTML)
+    cv.innerHTML=++a
+      }
+    })
+}
+
+function sub(e) {
+  e.parentElement.childNodes.forEach(function (cv) {
+  if(cv.classList.contains('leastVal')){
+    var a = parseInt(cv.innerHTML)
+   if(a>0){
+      cv.innerHTML=--a
+    }
+      }
+      
+    })
+    
+}
+
+function sortPrice() {
+  let sortPriceObj = food.slice().sort(function (a, b) {
+    return a.amount - b.amount;
+  });
+  let colsRemove = document.querySelectorAll(".col");
+  colsRemove.forEach((e) => {
+    e.remove();
+  });
+  itemRenderer(sortPriceObj);
+}
